95e2000e8a7e533532f822c002f23747dfb08415
aboczek <adam.boczekwork@gmail.com>
2022-10-08 17:50:07 +0000

    add code to increment score and incorrect answer tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,8 +46,10 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert(`Awww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 }
@@ -70,13 +72,20 @@ function calculateCorrectAnswer() {
     }
 
 }
-
+ /**
+  *  gets the current score from the DOM and increments it by 1
+  */
 function incrementScore() {
-
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+  *  gets the current incorrect answer from the DOM and increments it by 1
+  */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
